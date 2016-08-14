fc2c31783b30fe0d124d2202c51cf532c7458b34
Marc Brinkmann <git@marcbrinkmann.de>
2016-08-14 13:08:01 +0200

    Automatic commmit using gittar.

--- bresenham/sidebar-items.js
+++ bresenham/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Bresenham",""]],"type":[["Point",""]]});
+initSidebarItems({"struct":[["Bresenham","Line-drawing iterator"]],"type":[["Point","Convenient typedef for two machines-sized integers"]]});
--- search-index.js
+++ search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex["bresenham"] = {"doc":"","items":[[3,"Bresenham","bresenham","",null,null],[6,"Point","","",null,null],[11,"new","","",0,{"inputs":[{"name":"point"},{"name":"point"}],"output":{"name":"bresenham"}}],[11,"next","","",0,null]],"paths":[[3,"Bresenham"]]};
+searchIndex["bresenham"] = {"doc":"Iterator-based Bresenham&#39;s line drawing algorithm","items":[[3,"Bresenham","bresenham","Line-drawing iterator",null,null],[6,"Point","","Convenient typedef for two machines-sized integers",null,null],[11,"new","","Creates a new iterator.Yields intermediate points between `start`\nand `end`. Does include `start` but not `end`.",0,{"inputs":[{"name":"point"},{"name":"point"}],"output":{"name":"bresenham"}}],[11,"next","","",0,null]],"paths":[[3,"Bresenham"]]};
 initSearch(searchIndex);
